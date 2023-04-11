848d4258fb67875374b49765c382c723e8e533ac
Sirius Black <mariia.beliaev@gmail.com>
2023-04-11 21:23:33 +0300

    added two solutions for the problem

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 // Итерационный подсчет факториала
-function factIterat(num){
-
+function factIterat(num) {
+  let result = 1;
+  if (num < 0) {
+    alert("формула принимает только неотрицательное целое число");
+  } else if (num === 0) {
+    result = 1;
+  } else {
+    for (let i = 1; i <= num; i++) {
+      result = result * i;
+    }
+  }
+  return result;
 }
 
 // Рекурсивный расчет факториала
-function factRec(num){
- 
+function factRec(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factRec(num - 1);
+  }
 }
